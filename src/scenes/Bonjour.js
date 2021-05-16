c4231f070801707c5e59cb911073d3ba07e09a59
oumaima324 <oumaima.mechehoul@gmail.com>
2021-05-16 20:03:52 +0200

    add bonjour.js

---
+++ src/scenes/Bonjour.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react';
+import { View, FlatList,StyleSheet,Title} from 'react-native';
+
+
+
+
+
+class Bonjour extends Component {
+
+
+
+
+  render () {
+    return (
+      <View>
+      <title>TRACEURS</title>
+      </View>
+    )
+  }
+
+}
+
+
+export default Bonjour;
