d0c40ee52c551ad2b4090da5da369c63fdafac33
imane.97.echriki@gmail.com <imane.97.echriki@gmail.com>
2021-05-16 20:06:08 +0200

    ajout commit

---
+++ src/scenes/Commit.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import HeaderComponent from '../components/HeaderComponent';
+import Searchbar from '../components/Searchbar';
+
+class Commit extends Component {
+  render () {
+    return (
+      <View>
+
+      <HeaderComponent >  Evénements </HeaderComponent>
+
+      <View style = {styles.lineStyle} />
+
+
+      <Searchbar> </Searchbar>
+
+      </View>
+
+    )
+  }
+
+  }
+  const styles = StyleSheet.create({
+    lineStyle:{
+          borderWidth: 5,
+          borderColor:'#808080',
+          margin:0,
+        },
+          });
+
+  export default Commit;
